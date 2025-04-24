ea53b0647d94d595c2aab9a7a779ea2543c552cc
yves-cadour <yves.cadour@saint-louis29.net>
2025-04-24 02:58:51 +0200

    fisrt import

---
+++ src/arcade_game/game.py
@@ -0,0 +1,74 @@
+"""
+le module principal du projet arcade_game
+
+                  :: ::::: ::
+                  :: ::::: ::
+                  :: ::::: ::
+                  :: ::::: ::
+                  :: ::::: ::
+
+
+                 .:: ::::: ::.
+                .::: ::::: :::.
+               .:::' ::::: ':::.
+              .::::' ::::: '::::.
+             .::::'  :::::  '::::.
+           .:::::'   :::::   ':::::.
+         .::::::'    :::::    '::::::.
+    ...:::::::'      :::::      ':::::::...
+    :::::::''        :::::        '':::::::
+    ::::''           :::::           ''::::
+
+      _______ _______ _______ ______  ___
+     |   _   |       |   _   |   _  \|   |(R)
+     |.  1   |.|   | |.  1   |.  l   |.  |
+     |.  _   `-|.  |-|.  _   |.  _  <|.  |
+     |:  |   | |:  | |:  |   |:  |   |:  |
+     |::.|:. | |::.| |::.|:. |::.|:. |::.|
+     `--- ---' `---' `--- ---`--- ---`---'
+
+"""
+
+import pyxel
+from arcade_game.spaceship import Spaceship
+
+class Game:
+    """
+    Une classe pour notre jeu
+    """
+    def __init__(self):
+        """
+        Initialisation du jeu
+        """
+        self.w = 128 #largeur de l'écran
+        self.h = 256 #hauteur de l'écran
+        self.spaceship = Spaceship(self.w//2, self.h-8) #instanciation du vaisseau
+        pyxel.init(self.w, self.h, title="Arcade Game")
+        # chargement des images
+        pyxel.load("images.pyxres")
+        # --> appel de la fonction principale
+        pyxel.run(self.update, self.draw)
+
+    # =====================================================
+    # == UPDATE (30FPS)
+    # =====================================================
+    def update(self):
+        """mise à jour des variables (30 fois par seconde)"""
+
+        # deplacement du vaisseau
+        self.spaceship.update()
+
+
+    # =====================================================
+    # == DRAW (30FPS)
+    # =====================================================
+    def draw(self):
+        """création et positionnement des objets (30 fois par seconde)"""
+
+        # vide la fenetre 30 fois par seconde
+        pyxel.cls(0)
+
+        self.spaceship.draw()
+
+# instanciation de notre classe
+Game()
---
+++ src/arcade_game/spaceship.py
@@ -0,0 +1,69 @@
+"""
+Un module pour le vaisseau
+                                   _     _
+                                  | |   (_)
+     ___ _ __   __ _  ___ ___  ___| |__  _ _ __
+    / __| '_ \ / _` |/ __/ _ \/ __| '_ \| | '_ \
+    \__ \ |_) | (_| | (_|  __/\__ \ | | | | |_) |
+    |___/ .__/ \__,_|\___\___||___/_| |_|_| .__/
+        | |                               | |
+        |_|                               |_|
+                     `. ___
+                    __,' __`.                _..----....____
+        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'
+  _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'
+,'________________                          \`-._`-','
+ `._              ```````````------...___   '-.._'-:
+    ```--.._      ,.                     ````--...__\-.
+            `.--. `-`                       ____    |  |`
+              `. `.                       ,'`````.  ;  ;`
+                `._`.        __________   `.      \'__/`
+                   `-:._____/______/___/____`.     \  `
+                               |       `._    `.    \
+                               `._________`-.   `.   `.___
+                                                  `------'`
+"""
+
+import pyxel
+
+class Spaceship :
+    """
+    Une classe pour notre vaisseau
+    """
+    def __init__(self, x, y):
+        """Initialisation du vaisseau
+
+        :param x: L'abscisse du coin supérieur gauche
+        :type x: int
+        :param y: L'ordonnée du coin supérieur gauche
+        :type y: int
+        """
+        # position initiale du vaisseau
+        self.x = x
+        self.y = y
+        # largeur (width) et hauteur du vaisseau (height)
+        self.w = 8
+        self.h = 8
+
+    # =====================================================
+    # == UPDATE
+    # =====================================================
+    def update(self):
+        """Mise à jour du vaisseau (30FPS)
+        """
+        self._move()
+
+    def _move(self):
+        """déplacement avec les touches de directions"""
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.x += 1
+
+    # =====================================================
+    # == UDRAX
+    # =====================================================
+    def draw(self):
+        """
+        Dessin du vaisseau
+        """
+        # vaisseau (carre 8x8)
+        pyxel.blt(self.x, self.y, 0, 0, 0, 8, 8)
